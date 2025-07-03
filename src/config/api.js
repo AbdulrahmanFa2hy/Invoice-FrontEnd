169b29016c87d00a9451ecdf09967096d51cb9a3
AbdulrahmanFa2hy <abdelrhman.fa2hy@gmail.com>
2025-07-03 20:39:44 +0300

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,10 +1,10 @@
 // API Configuration
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  "https://ajayb.up.railway.app/api/v1";
+  "https://invoice-backend-production-3e61.up.railway.app/api/v1";
 
 const UPLOADS_BASE_URL =
   import.meta.env.VITE_UPLOADS_BASE_URL ||
-  "https://ajayb.up.railway.app/uploads";
+  "https://invoice-backend-production-3e61.up.railway.app/uploads";
 
 export { API_BASE_URL, UPLOADS_BASE_URL };
